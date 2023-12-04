0304451f4220bbb346602045b2740643709c4003
Lakshman <lakshmanyadavpatakula@gmail.com>
2023-12-03 18:31:06 -0700

    4.3

--- xv6-riscv/kernel/trap-and-emulate.c
+++ xv6-riscv/kernel/trap-and-emulate.c
@@ -219,8 +219,6 @@ void trap_and_emulate(void) {
 
             p->trapframe->epc += 4;
             return;  // Exit the loop since we found and processed the matching uimm
-        }else {
-            setkilled(p);
         }
     }
 
