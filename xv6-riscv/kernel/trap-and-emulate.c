87eec1afe51032b1eca8be4608db3a89e89d2102
Lakshman <lakshmanyadavpatakula@gmail.com>
2023-12-03 18:45:32 -0700

    4.4

--- xv6-riscv/kernel/trap-and-emulate.c
+++ xv6-riscv/kernel/trap-and-emulate.c
@@ -191,10 +191,10 @@ void trap_and_emulate(void) {
     }else if (funct3 == 0x1) {
         //csrw
         /* Print the statement */
-        // printf("(PI at %p) op = %x, rd = %x, funct3 = %x, rs1 = %x, uimm = %x\n", 
-        //         addr, op, rd, funct3, rs1, uimm);
+        printf("(PI at %p) op = %x, rd = %x, funct3 = %x, rs1 = %x, uimm = %x\n", 
+                addr, op, rd, funct3, rs1, uimm);
         // for (int i=0 ; i<36 ; i++) {
-        // if (vm_state.totalregs[i].code == uimm) {
+        // if (vm_state.totalregs[i].code == uimm && vm_state.exec_mode >= vm_state.totalregs[i].mode ) {
         //     // if (vm_state.exec_mode >= vm_state.totalregs[i].mode) {
         //         uint64* bp = rs1 + &(p->trapframe->ra) - 1;
         //         vm_state.totalregs[i].val = (*bp);
@@ -217,8 +217,8 @@ void trap_and_emulate(void) {
 
 
 
-            p->trapframe->epc += 4;
-
+            // p->trapframe->epc += 4;
+            return;  // Exit the loop since we found and processed the matching uimm
         }
     }
 
@@ -240,7 +240,7 @@ void trap_and_emulate(void) {
 
     
 
-    // p->trapframe->epc += 4;
+    p->trapframe->epc += 4;
     }
     else if(funct3 == 0x2){
         //csrr
